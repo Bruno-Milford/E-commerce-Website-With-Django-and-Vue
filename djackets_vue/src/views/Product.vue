3f0400871345600164ae024b635f2ec658eeb203
Trynda-LOL <bm33518@gmail.com>
2021-08-27 11:16:08 -0300

    loading bar

--- djackets_vue/src/views/Product.vue
+++ djackets_vue/src/views/Product.vue
@@ -40,15 +40,19 @@ export default {
     this.getProduct()
   },
   methods: {
-    getProduct() {
+    async getProduct() {
+      this.$store.commit('setIsLoading', true)
+
       const category_slug = this.$route.params.category_slug
       const product_slug = this.$route.params.product_slug
 
-      axios.get(`/api/v1/products/${ category_slug }/${ product_slug }`).then(response => {
+      await axios.get(`/api/v1/products/${ category_slug }/${ product_slug }`).then(response => {
         this.product = response.data
       }).catch(error => {
         console.error(error)
       })
+
+      this.$store.commit('setIsLoading', false)
     },
     addToCart() {
       if (isNaN(this.quantity) || this.quantity < 1) {
